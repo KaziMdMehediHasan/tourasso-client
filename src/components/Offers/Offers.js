b0073b795dba63d18cd4b16e4e11f12fb326d478
= <=>
2021-10-31 19:19:19 +0600

    spinner added

--- src/components/Offers/Offers.js
+++ src/components/Offers/Offers.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
 import SingleOffer from "../SingleOffer/SingleOffer";
 import "./Offers.css";
 
 const Offers = () => {
+  const { user, isLoading } = useAuth();
   const [offers, setOffers] = useState([]);
   useEffect(() => {
     fetch("https://fast-meadow-84110.herokuapp.com/offers")
@@ -12,6 +14,18 @@ const Offers = () => {
         setOffers(data);
       });
   }, []);
+
+  //spinner
+  if (isLoading) {
+    return (
+      <div className="d-flex justify-content-center align-items-center">
+        <div class="spinner-border text-danger" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="mt-5">
       <h1 className="text-center my-5">Our Most Popular And Best Adventures</h1>
